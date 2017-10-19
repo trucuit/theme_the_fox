cafec0cae8ef342863217e0f468ac2b100e116e0
TRỰC TRẦN TRUNG <15520939@gm.uit.edu.vn>
2017-10-19 14:38:56 +0700

    commit layout

---
+++ 1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title> Example </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src='vendor/jquery-3.2.1.min.js'></script>
+	<script type="text/javascript" src="vendor/bootstrap.js"></script>
+	<script type="text/javascript" src="1.js"></script>
+	<link rel="stylesheet" href="vendor/bootstrap.css">
+	<link rel="stylesheet" href="vendor/font-awesome.css">
+	<link rel="stylesheet" href="1.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">	
+</head>
+<body>
+	<?php
+	include_once 'file/header.html';
+	include_once 'file/menu.html';
+	include_once 'file/product.html';
+	include_once 'file/info.html';
+	include_once 'file/blog.html';
+// include_once 'file/scandir(directory)lideshow-post.html';
+	include_once 'file/special.html';
+	include_once 'file/feature-post.html';
+	?>
+</div>
+</nav>
+</body>
+</html>
